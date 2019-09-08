f24c470c4b98650af7923383f425bfa770b5f7af
eurusov <eurusov@users.noreply.github.com>
2019-09-08 15:29:45 +0300

    Initial commit.

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.Car;
+import org.hibernate.SessionFactory;
+import util.DBHelper;
+
+import java.util.List;
+
+public class CarService {
+
+    private static CarService carService;
+
+    private SessionFactory sessionFactory;
+
+    private CarService(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public static CarService getInstance() {
+        if (carService == null) {
+            carService = new CarService(DBHelper.getSessionFactory());
+        }
+        return carService;
+    }
+
+
+    public String getAllCars() {
+        return null;
+    }
+}
