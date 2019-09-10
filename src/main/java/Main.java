534fe274a5d95c2ff7bc4b4ef3a727dc1cbf54c6
eurusov <eurusov@users.noreply.github.com>
2019-09-10 15:56:23 +0300

    Some testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -32,8 +32,8 @@ public class Main {
                 "BD51 SMR",
                 45000L
         );
-        System.out.println("id of added car: " + carId);
-        carService.sellCar(carId); // mark this car as sold
+        System.out.println("Car was added with id = " + carId);
+//        carService.sellCar(carId); // mark this car as sold
         SimpleCar car = carService.getCar(carId);
         System.out.println(car);
 
@@ -43,28 +43,31 @@ public class Main {
                 "BND007",
                 55000L
         );
-        System.out.println("id of added car: " + carId);
-        SimpleCar car2 = carService.getCar(carId);
-        System.out.println(car2);
+        System.out.println("Car was added with id = " + carId);
+        car = carService.getCar(carId);
+        System.out.println(car);
         System.out.println();
 
-        System.exit(0);
 
-        dailyReportService.addDailyReport(123000L, 16L);
+        Long repId = dailyReportService.addDailyReport(123000L, 16L);
+        System.out.println("DailyReport was added with id = " + repId);
+        SimpleReport simpleReport = dailyReportService.getDailyReport(repId);
+        System.out.println("getDailyReport by this id returns: " + simpleReport);
+
         DBDate.nextDay();
-        long repId = dailyReportService.addDailyReport(3000L, 10L);
-        System.out.println("Added report id: " + repId);
-        SimpleReport report = dailyReportService.getDailyReport(repId);
-        System.out.println("Report data set: " + report);
+        System.out.println("\nDate was changed.\n");
+
+        repId = dailyReportService.addDailyReport(3000L, 10L);
+        System.out.println("DailyReport was added with id = " + repId);
+        simpleReport = dailyReportService.getDailyReport(repId);
+        System.out.println("getDailyReport by this id returns: " + simpleReport);
+
+        System.out.println("\nAll not sold cars list:\n" + carService.getAllCars());
+        System.out.println("\nAll reports list:\n" + dailyReportService.getAllDailyReports());
+        System.out.println("\nLast report:\n" + dailyReportService.getLastReport());
         System.out.println();
 
-        System.out.println(carService.getAllCars());
-
-        DailyReportService drSrv = DailyReportService.getInstance();
-        System.out.println(drSrv.getAllDailyReports());
-        System.out.println(drSrv.getLastReport());
-
-        DBHelper.deleteAll();
+//        DBHelper.deleteAll();
 
 //            sessionFactory.close();
 
